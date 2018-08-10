054ce7131c63908e906fccc4a0b0330631977540
datapython <lovenature_2008@yahoo.com>
2018-08-10 08:30:50 -0400

    del a func for simplification by using dict.get

--- vcf_only.py
+++ vcf_only.py
@@ -35,22 +35,12 @@ def get_info_column_value():
     info = vcf['INFO']
     info = [i.split(';') for i in info]
     info = [[j.split('=') for j in k] for k in info]
-    
+    infos =[[(i[0], i[1]) if (len(i) > 1) else (i[0], i[0]) for i in j] for j in info]
+
     # Collect all unique values from all rows of INFO column.
     all_values = []
     [all_values.append(a[0]) for b in info for a in b if a[0] not in all_values]
-    return (info, all_values)
-
-
-def transform_info_column():
-    info = get_info_column_value()[0]
-    values = get_info_column_value()[1]
-
-    # Fill out all unique values for all the rows. The missing ones take NaN.
-    [k.append([l, 'NaN']) for l in values for k in info if l not in [m[0] for m in k]]
-
-    infos =[[(i[0], i[1]) if (len(i) > 1) else (i[0], i[0]) for i in j] for j in info]
-    return (values, infos)
+    return (infos, all_values)
 
 
 def update_vcf():
@@ -58,13 +48,13 @@ def update_vcf():
     Update vcf dataframe file by adding new columns. Each of them represents a INFO value appeared at least once in the vcf dataset.
     """
     vcf = read_vcf(file)[1]
-    cNames = transform_info_column()[0]
-    xcols = transform_info_column()[1]   
+    cNames = get_info_column_value()[1]
+    xcols = get_info_column_value()[0]   
     xcols = [dict(row) for row in xcols]
 
     # Add new columns to vcf dataframe.
     for name in cNames:
-        vcf[name] = [r[name] for r in xcols]
+        vcf[name] = [r.get(name, 'NaN') for r in xcols]
     return vcf
 
 
